d7f5b4b42cafec82a3b92ab7c37f2fc63379ecb3
LucaPinheiro <lucapgomes11@gmail.com>
2024-03-17 00:56:52 -0300

    finishing create user controller✅

--- src/modules/user/create_user/app/create_user_controller.ts
+++ src/modules/user/create_user/app/create_user_controller.ts
@@ -1,26 +1,19 @@
 import { Request, Response } from "express";
 import { CreateUserUsecase } from "./create_user_usecase";
-import {
-  MissingParameters,
-  WrongTypeParameters,
-} from "../../../../shared/helpers/errors/controller_errors";
 import { EntityError } from "../../../../shared/helpers/errors/domain_errors";
-import {
-  ForbiddenAction,
-  NoItemsFound,
-} from "../../../../shared/helpers/errors/usecase_errors";
-import {
-  NotFound,
-  BadRequest,
-  Forbidden,
-  InternalServerError,
-} from "http-errors";
+import { ForbiddenAction, NoItemsFound } from "../../../../shared/helpers/errors/usecase_errors";
+import { BadRequest, Forbidden, InternalServerError } from "http-errors";
+import { UserProps } from "../../../../shared/domain/entities/user";
+import { MissingParameters } from "../../../../shared/helpers/errors/controller_errors";
 
 export class CreateUserController {
-  static async createUser(req: Request, res: Response) {
+  constructor(private createUserUsecase: CreateUserUsecase) {}
+
+  async createUser(req: Request, res: Response) {
     try {
       console.log("TENTANDO CRIAR USUÁRIO");
       const { name, email, role, password } = req.body;
+
       const errors = [];
 
       if (!name) {
@@ -40,30 +33,15 @@ export class CreateUserController {
       }
 
       if (errors.length > 0) {
-        return res
-          .status(400)
-          .json(errors.map((error) => ({ message: error.message })));
+        return res.status(400).json(errors);
       }
 
-      const createUserUsecase = new CreateUserUsecase();
-      const newUser = await createUserUsecase.createUser({
-        name,
-        email,
-        role,
-        password,
-      });
+      const userProps: UserProps = { name, email, role, password };
+      const newUser = await this.createUserUsecase.execute(userProps);
+
       res.status(201).json(newUser);
     } catch (error: any) {
-      if (error instanceof NoItemsFound) {
-        return res.status(404).json(new NotFound(error.message));
-      }
-      if (error instanceof MissingParameters) {
-        return res.status(400).json(new BadRequest(error.message));
-      }
-      if (error instanceof WrongTypeParameters) {
-        return res.status(400).json(new BadRequest(error.message));
-      }
-      if (error instanceof EntityError) {
+      if (error instanceof NoItemsFound || error instanceof EntityError) {
         return res.status(400).json(new BadRequest(error.message));
       }
       if (error instanceof ForbiddenAction) {
