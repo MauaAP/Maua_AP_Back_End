52f9c4f4eaa3d66441d3b01d5ac2b4c1feede421
LucaPinheiro <lucapgomes11@gmail.com>
2024-03-19 00:05:46 -0300

    adding getUserByEmail✅

--- src/shared/domain/repositories/user_repository_interface.ts
+++ src/shared/domain/repositories/user_repository_interface.ts
@@ -3,5 +3,6 @@ import { User } from "../entities/user";
 export interface IUserRepository {
 
   createUser(user: User): Promise<User>;
+  getUserByEmail(email: string): Promise<User | undefined>;
 
 }
