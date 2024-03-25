098aff20bf79d34918d909c20a21635e76e7cffd
LucaPinheiro <lucapgomes11@gmail.com>
2024-03-24 23:50:43 -0300

    adjusting get all users routes and verify in controller if role is admin✅

--- src/modules/user/get_all_users/get_all_users_routes.ts
+++ src/modules/user/get_all_users/get_all_users_routes.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from "express";
 import { UserRepositoryPrisma } from "../../../shared/infra/repositories/user_repository_prisma";
 import { GetAllUsersUsecase } from "./app/get_all_users_usecase";
 import { GetAllUsersController } from "./app/get_all_users_controller";
-import { authenticateToken } from "../../../shared/middlewares/jwt_middleware"; // Corrigir a importação
+import { authenticateToken } from "../../../shared/middlewares/jwt_middleware"; 
 
 const router = express.Router();
 
@@ -10,10 +10,9 @@ const userRepository = new UserRepositoryPrisma();
 const getAllUsersUsecase = new GetAllUsersUsecase(userRepository);
 const getAllUsersController = new GetAllUsersController(getAllUsersUsecase);
 
-// Utilizando o middleware authenticateToken
 router.get(
   "/users",
-  authenticateToken, // Corrigir o nome do middleware
+  authenticateToken,
   async (req: Request, res: Response) => {
     await getAllUsersController.handle(req, res);
   }
