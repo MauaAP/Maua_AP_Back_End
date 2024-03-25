7164c7a2d41a5606fde9c8a5763857ff782a9f6d
LucaPinheiro <lucapgomes11@gmail.com>
2024-03-24 22:33:52 -0300

    changing middleware

--- src/modules/user/get_all_users/get_all_users_routes.ts
+++ src/modules/user/get_all_users/get_all_users_routes.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from "express";
 import { UserRepositoryPrisma } from "../../../shared/infra/repositories/user_repository_prisma";
 import { GetAllUsersUsecase } from "./app/get_all_users_usecase";
 import { GetAllUsersController } from "./app/get_all_users_controller";
-import { authenticateAdminToken } from "../../../shared/middlewares/jwt_admin_middleware";
+import { authenticateToken } from "../../../shared/middlewares/jwt_middleware"; // Corrigir a importação
 
 const router = express.Router();
 
@@ -10,8 +10,13 @@ const userRepository = new UserRepositoryPrisma();
 const getAllUsersUsecase = new GetAllUsersUsecase(userRepository);
 const getAllUsersController = new GetAllUsersController(getAllUsersUsecase);
 
-router.get("/users", authenticateAdminToken, async (req: Request, res: Response) => {
-  await getAllUsersController.handle(req, res);
-});
+// Utilizando o middleware authenticateToken
+router.get(
+  "/users",
+  authenticateToken, // Corrigir o nome do middleware
+  async (req: Request, res: Response) => {
+    await getAllUsersController.handle(req, res);
+  }
+);
 
 export default router;
