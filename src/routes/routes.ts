e4e783852a9f42b3b3a4c0a3fe145db7baad854a
LucaPinheiro <lucapgomes11@gmail.com>
2024-03-26 14:05:17 -0300

    adding update route

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -3,6 +3,7 @@ import CreateUserRoutes from "../modules/user/create_user/create_user_routes";
 import AuthUserRoutes from "../modules/user/auth_user/auth_user_routes";
 import GetAllUserRoutes from "../modules/user/get_all_users/get_all_users_routes"
 import GetUserByEmailRoutes from "../modules/user/get_user_by_email/get_user_by_email_routes";
+import UpdateUserRoutes from "../modules/user/update_user/update_user_routes"
 const routes = (app: Express) => {
   app
     .route("/")
@@ -18,6 +19,7 @@ const routes = (app: Express) => {
   app.use("/api", AuthUserRoutes);
   app.use("/api", GetAllUserRoutes);
   app.use("/api", GetUserByEmailRoutes)
+  app.use("/api", UpdateUserRoutes)
 };
 
 export default routes;
