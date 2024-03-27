103f61002c0fb083837852979e35027a802d9747
LucaPinheiro <lucapgomes11@gmail.com>
2024-03-26 22:36:02 -0300

    adding MODERATOR and PROFESSOR role✅

--- src/shared/domain/enums/role_enum.ts
+++ src/shared/domain/enums/role_enum.ts
@@ -1,6 +1,8 @@
 export enum ROLE {
   USER = "USER",
   ADMIN = "ADMIN",
+  MODERATOR = "MODERATOR",
+  PROFESSOR = "PROFESSOR",
 }
 
 export function toEnum(value: string): ROLE {
@@ -9,6 +11,10 @@ export function toEnum(value: string): ROLE {
       return ROLE.USER;
     case "ADMIN":
       return ROLE.ADMIN;
+    case "MODERATOR":
+      return ROLE.MODERATOR;
+    case "PROFESSOR":
+      return ROLE.PROFESSOR;
     default:
       throw new Error("Invalid value");
   }
