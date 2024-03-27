678dd5b5edd7d2bdd4d9891cf6ea5a622d054904
LucaPinheiro <lucapgomes11@gmail.com>
2024-03-27 10:07:43 -0300

    adding get all events presenter to routes✅

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -4,6 +4,7 @@ import AuthUserPresenter from "../modules/user/auth_user/app/auth_user_presenter
 import GetAllUserPresenter from "../modules/user/get_all_users/app/get_all_users_presenter";
 import GetUserByEmailPresenter from "../modules/user/get_user_by_email/app/get_user_by_email_presenter";
 import CreateEventPresenter from "../modules/event/create_event/app/create_event_presenter";
+import GetAllEventsPresenter from "../modules/event/get_all_events/app/get_all_events_presenter";
 const routes = (app: Express) => {
   app
     .route("/")
@@ -16,7 +17,6 @@ const routes = (app: Express) => {
     );
 
   // user routes
-
   app.use("/api", CreateUserPresenter);
   app.use("/api", AuthUserPresenter);
   app.use("/api", GetAllUserPresenter);
@@ -24,6 +24,7 @@ const routes = (app: Express) => {
 
   // event routes
   app.use("/api", CreateEventPresenter);
+  app.use("/api", GetAllEventsPresenter);
 };
 
 export default routes;
