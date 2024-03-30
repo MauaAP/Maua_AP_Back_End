44cd3a06e707e5088173c5fc1cba5b3732d84bc3
LucaPinheiro <lucapgomes11@gmail.com>
2024-03-30 16:17:36 -0300

    adjusting messages errors👍

--- src/modules/user/get_all_users/app/get_all_users_controller.ts
+++ src/modules/user/get_all_users/app/get_all_users_controller.ts
@@ -2,18 +2,25 @@ import { GetAllUsersUsecase } from "./get_all_users_usecase";
 import { Request, Response } from "express";
 import { UserFromToken } from "../../../../shared/middlewares/jwt_middleware";
 import { GetAllUsersViewmodel } from "./get_all_users_viewmodel";
+import {
+  ForbiddenAction,
+  NoItemsFound,
+} from "../../../../shared/helpers/errors/usecase_errors";
+import { EntityError } from "../../../../shared/helpers/errors/domain_errors";
+import {
+  MissingParameters,
+  WrongTypeParameters,
+} from "../../../../shared/helpers/errors/controller_errors";
 
 export class GetAllUsersController {
   constructor(private usecase: GetAllUsersUsecase) {}
 
   async handle(req: Request, res: Response) {
     try {
-      const userFromToken = req.user as UserFromToken; 
+      const userFromToken = req.user as UserFromToken;
 
       if (userFromToken.role !== "ADMIN") {
-        return res
-          .status(403)
-          .json({ error: "Acesso negado." });
+        return res.status(403).json({ error: "Acesso negado." });
       }
 
       const users = await this.usecase.execute();
@@ -22,7 +29,21 @@ export class GetAllUsersController {
       );
       res.status(200).json(usersViewModel);
     } catch (error: any) {
-      return res.status(500).send({ error: error.message });
+      if (error instanceof NoItemsFound) {
+        return res.status(404).json({ error: error.message });
+      }
+      if (
+        error instanceof WrongTypeParameters ||
+        error instanceof EntityError
+      ) {
+        return res.status(400).json({ error: error.message });
+      }
+      if (error instanceof ForbiddenAction) {
+        return res.status(401).json({ error: error.message });
+      }
+      if (error instanceof Error) {
+        return res.status(500).json({ error: error.message });
+      }
     }
   }
 }
