592deb5b87f0840a3ea7114220924074047a20cd
LucaPinheiro <lucapgomes11@gmail.com>
2024-04-23 14:14:26 -0300

    adding get user by id in interface✅

--- src/shared/domain/repositories/user_repository_interface.ts
+++ src/shared/domain/repositories/user_repository_interface.ts
@@ -5,5 +5,6 @@ export interface IUserRepository {
   createUser(user: User): Promise<User>;
   getUserByEmail(email: string): Promise<User | undefined>;
   getAll(): Promise<User[]>;
+  getUserById(id: string): Promise<User | undefined>;
 
 }
