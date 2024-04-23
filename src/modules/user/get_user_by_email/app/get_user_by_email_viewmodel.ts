ab30083dc052f47c331f7184b546170a48affa32
LucaPinheiro <lucapgomes11@gmail.com>
2024-04-23 14:13:39 -0300

    include user id in return of viewmodel✅

--- src/modules/user/get_user_by_email/app/get_user_by_email_viewmodel.ts
+++ src/modules/user/get_user_by_email/app/get_user_by_email_viewmodel.ts
@@ -1,12 +1,14 @@
 import { User } from "../../../../shared/domain/entities/user";
 
 export class GetUserByEmailViewmodel {
+    id: string;
     name: string;
     email: string;
     role: string;
     status: string;
     
     constructor(user: User) {
+        this.id = user.userId ?? '';
         this.name = user.name;
         this.email = user.email;
         this.role = user.role;
