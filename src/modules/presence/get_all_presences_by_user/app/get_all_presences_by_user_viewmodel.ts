8fbb53ed83c2fec29926ef619dc12c0743b8dc2d
LucaPinheiro <lucapgomes11@gmail.com>
2024-04-28 02:06:54 -0300

    Add GetAllPresencesByUserViewmodel to handle the view model for retrieving all presences by user

---
+++ src/modules/presence/get_all_presences_by_user/app/get_all_presences_by_user_viewmodel.ts
@@ -0,0 +1,15 @@
+import { Presence } from "../../../../shared/domain/entities/presence";
+
+export class GetAllPresencesByUserViewmodel {
+  presenceId: string;
+  eventId: string;
+  userId: string;
+  date: number;
+
+  constructor(presence: Presence) {
+    this.presenceId = presence.presenceId;
+    this.eventId = presence.eventId;
+    this.userId = presence.userId;
+    this.date = presence.date;
+  }
+}
