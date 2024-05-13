1936a6e221a5e0ccc53a7c7401ce0144ab88e927
LucaPinheiro <lucapgomes11@gmail.com>
2024-05-13 15:19:46 -0300

    feat: Update S3 bucket name in certificate_repository_s3.ts

--- src/shared/infra/repositories/certificate_repository_s3.ts
+++ src/shared/infra/repositories/certificate_repository_s3.ts
@@ -1,35 +1,43 @@
-// import { S3Client } from "@aws-sdk/client-s3";
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { AwsCredentialIdentity } from "@aws-sdk/types";
 
-// // const s3 = new AWS.S3({
-// //   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-// //   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-// //   region: process.env.REGION,
-// // });
+// Obtém as credenciais do ambiente
+const credentials: AwsCredentialIdentity = {
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID || "",
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || ""
+};
 
-// export async function saveCertificate(
-//   userId: string,
-//   eventId: string,
-//   certificatePdf: Buffer
-// ): Promise<string> {
-//   console.log("Certificate PDF type:", typeof certificatePdf); 
-//   console.log("Certificate PDF length:", certificatePdf.length); 
+// Verifica se a região está definida
+const region = process.env.REGION || "us-east-1";
 
-//   const params = {
-//     Bucket: `${process.env.BUCKET_NAME}`,
-//     Key: `${userId}-${eventId}-certificado.pdf`,
-//     Body: certificatePdf
-//   };
-//   console.log(params.Key);
-//   try {
-//     console.log("Uploading certificate to S3...");
-//     const data = await s3.putObject(params).promise();
-//     console.log("Certificate uploaded successfully. Data:", data);
-//     const certificateUrl = `https://${process.env.BUCKET_NAME}.s3.${
-//         process.env.REGION
-//       }.amazonaws.com/${userId}-${eventId}-certificado.pdf`
-//     return certificateUrl;
-//   } catch (error: any) {
-//     console.error("Error uploading certificate to S3:", error);
-//     throw new Error(`Erro ao salvar certificado no S3: ${error.message}`);
-//   }
-// }
+const s3 = new S3Client({
+  credentials: credentials,
+  region: region
+});
+
+export async function saveCertificate(
+  userId: string,
+  eventId: string,
+  certificatePdf: Buffer
+): Promise<string> {
+  console.log("Certificate PDF type:", typeof certificatePdf); 
+  console.log("Certificate PDF length:", certificatePdf.length); 
+
+  const params = {
+    Bucket: `${process.env.BUCKET_NAME}`,
+    Key: `${userId}-${eventId}-certificado.pdf`,
+    Body: certificatePdf
+  };
+  console.log(params.Key);
+  try {
+    console.log("Uploading certificate to S3...");
+    const command = new PutObjectCommand(params);
+    const data = await s3.send(command);
+    console.log("Certificate uploaded successfully. Data:", data);
+    const certificateUrl = `https://${process.env.BUCKET_NAME}.s3.${process.env.REGION}.amazonaws.com/${userId}-${eventId}-certificado.pdf`
+    return certificateUrl;
+  } catch (error: any) {
+    console.error("Error uploading certificate to S3:", error);
+    throw new Error(`Erro ao salvar certificado no S3: ${error.message}`);
+  }
+}
