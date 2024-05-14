81afa67672fbe0e25cd1d584706387fed9cffc87
LucaPinheiro <lucapgomes11@gmail.com>
2024-05-14 17:15:06 -0300

    feat: Add email property to GetAllUsersToPresenceListViewmodel

--- src/modules/user/get_all_users_to_presence_list/app/get_all_users_to_presence_list_viewmodel.ts
+++ src/modules/user/get_all_users_to_presence_list/app/get_all_users_to_presence_list_viewmodel.ts
@@ -3,9 +3,11 @@ import { User } from "../../../../shared/domain/entities/user";
 export class GetAllUsersToPresenceListViewmodel {
   id: string;
   name: string;
+  email: string;
 
   constructor(user: User) {
     this.id = user.userId ?? "";
     this.name = user.name;
+    this.email = user.email;
   }
 }
