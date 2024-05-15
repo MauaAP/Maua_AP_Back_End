8ce48e874740492b3cc46e2071b5759dee6add58
LucaPinheiro <lucapgomes11@gmail.com>
2024-05-14 22:29:18 -0300

    chore: Update server.ts to listen on port 3015

--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import app from "./src/app";
 import routes from "./src/routes/routes";
 
-const PORT = 3000;
+const PORT = 3015;
 
 app.listen(PORT, () => {
   console.log("Servidor rodando na porta 3015");
