b22df9690421d48dee7610f0bd38dabdde45b97e
Rodrigo Bossini <professorbossini@gmail.com>
2024-06-07 17:27:55 -0300

    fix: tries to use dynamically installed chome

--- src/modules/presence/create_certificate/app/create_certificate_usecase.ts
+++ src/modules/presence/create_certificate/app/create_certificate_usecase.ts
@@ -60,7 +60,11 @@ export class CreateCertificateUsecase {
 
     const htmlString = getCertificateHtml(json);
 
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+      headless: true,
+      executablePath: '/usr/bin/google-chrome-stable',
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    });
     const page = await browser.newPage();
     await page.setContent(htmlString);
 
