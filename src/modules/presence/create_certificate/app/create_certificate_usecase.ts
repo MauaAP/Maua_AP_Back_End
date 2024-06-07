c2ab573bce69d6607ea85b698d596205880f629c
Rodrigo Bossini <professorbossini@gmail.com>
2024-06-07 18:08:44 -0300

    fix: returns puppeteer to use standard chrome

--- src/modules/presence/create_certificate/app/create_certificate_usecase.ts
+++ src/modules/presence/create_certificate/app/create_certificate_usecase.ts
@@ -60,11 +60,7 @@ export class CreateCertificateUsecase {
 
     const htmlString = getCertificateHtml(json);
 
-    const browser = await puppeteer.launch({
-      headless: true,
-      executablePath: '/usr/bin/google-chrome-stable',
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    });
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.setContent(htmlString);
 
