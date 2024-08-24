fe6bcdb05a7c168a3e6a6f449034017ed76a5149
LucaPinheiro <lucapgomes11@gmail.com>
2024-08-24 12:25:57 -0300

    refactor: Improve error handling in GetAllEventsController

--- src/modules/event/get_all_events/app/get_all_events_controller.ts
+++ src/modules/event/get_all_events/app/get_all_events_controller.ts
@@ -4,14 +4,17 @@ import { UserFromToken } from "../../../../shared/middlewares/jwt_middleware";
 import { GetAllEventsViewmodel } from "./get_all_events_viewmodel";
 import { EntityError } from "../../../../shared/helpers/errors/domain_errors";
 import {
-  MissingParameters,
-  WrongTypeParameters,
+  InvalidParameter,
+  InvalidRequest,
 } from "../../../../shared/helpers/errors/controller_errors";
-import {
-  ForbiddenAction,
-  NoItemsFound,
-} from "../../../../shared/helpers/errors/usecase_errors";
+import { NoItemsFound } from "../../../../shared/helpers/errors/usecase_errors";
 import { GetAllEventsUsecase } from "./get_all_events_usecase";
+import {
+  BadRequest,
+  Forbidden,
+  InternalServerError,
+  ParameterError,
+} from "../../../../shared/helpers/http/http_codes";
 
 export class GetAllEventsController {
   constructor(private repo: GetAllEventsUsecase) {}
@@ -21,29 +24,31 @@ export class GetAllEventsController {
       const userFromToken = req.user as UserFromToken;
 
       if (!userFromToken) {
-        return res.status(403).json({ error: "Acesso negado." });
+        throw new Forbidden(
+          "You do not have permission to access this feature"
+        );
       }
 
       const events = await this.repo.execute();
       const viewmodel = events.map((event) => new GetAllEventsViewmodel(event));
       res.status(200).json(viewmodel);
     } catch (error: any) {
+      if (error instanceof InvalidRequest) {
+        return new BadRequest(error.message).send(res);
+      }
+      if (error instanceof InvalidParameter) {
+        return new ParameterError(error.message).send(res);
+      }
+      if (error instanceof EntityError) {
+        return new ParameterError(error.message).send(res);
+      }
+      if (error instanceof Forbidden) {
+        return new Forbidden(error.getMessage()).send(res);
+      }
       if (error instanceof NoItemsFound) {
-        return res.status(404).json({ error: error.message });
+        return new Forbidden(error.message).send(res);
       }
-      if (
-        error instanceof MissingParameters ||
-        error instanceof WrongTypeParameters ||
-        error instanceof EntityError
-      ) {
-        return res.status(400).json({ error: error.message });
-      }
-      if (error instanceof ForbiddenAction) {
-        return res.status(401).json({ error: error.message });
-      }
-      if (error instanceof Error) {
-        return res.status(500).json({ error: error.message });
-      }
+      return new InternalServerError("Internal Server Error").send(res);
     }
   }
 }
