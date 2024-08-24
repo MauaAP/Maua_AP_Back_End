8f12b28972f3658fa9b230ef395d1059b2db45a8
LucaPinheiro <lucapgomes11@gmail.com>
2024-08-24 12:26:01 -0300

    refactor: Improve error handling in GetEventByIdController

--- src/modules/event/get_event_by_id/app/get_event_by_id_controller.ts
+++ src/modules/event/get_event_by_id/app/get_event_by_id_controller.ts
@@ -1,6 +1,15 @@
 import { Request, Response } from "express";
 import { GetEventByIdUsecase } from "./get_event_by_id_usecase";
 import { GetEventByIdViewmodel } from "./get_event_by_id_viewmodel";
+import {
+  InvalidParameter,
+  InvalidRequest,
+} from "../../../../shared/helpers/errors/controller_errors";
+import {
+  BadRequest,
+  InternalServerError,
+  ParameterError,
+} from "../../../../shared/helpers/http/http_codes";
 
 export class GetEventByIdController {
   constructor(private getEventByIdUsecase: GetEventByIdUsecase) {}
@@ -12,8 +21,13 @@ export class GetEventByIdController {
       const viewmodel = new GetEventByIdViewmodel(event);
       return res.status(200).json(viewmodel);
     } catch (error: any) {
-      console.error("Erro ao buscar evento por ID:", error.message);
-      return res.status(500).json({ error: error.message });
+      if (error instanceof InvalidRequest) {
+        return new BadRequest(error.message).send(res);
+      }
+      if (error instanceof InvalidParameter) {
+        return new ParameterError(error.message).send(res);
+      }
+      return new InternalServerError("Internal Server Error").send(res);
     }
   }
 }
