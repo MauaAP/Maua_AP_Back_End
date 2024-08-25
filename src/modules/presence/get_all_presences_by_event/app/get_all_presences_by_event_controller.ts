f4b9092d91ed9600e55d5054e5d7a08d68fb9c00
LucaPinheiro <lucapgomes11@gmail.com>
2024-08-25 11:50:29 -0300

    refactor: Improve error handling in GetAllPresencesByEventController.ts

--- src/modules/presence/get_all_presences_by_event/app/get_all_presences_by_event_controller.ts
+++ src/modules/presence/get_all_presences_by_event/app/get_all_presences_by_event_controller.ts
@@ -1,11 +1,15 @@
 import { Request, Response } from "express";
 import { GetAllPresencesByEventUsecase } from "./get_all_presences_by_event_usecase";
 import { UserFromToken } from "../../../../shared/middlewares/jwt_middleware";
-import { BadRequest, Forbidden, InternalServerError } from "http-errors";
+import {
+  ParameterError,
+  BadRequest,
+  InternalServerError,
+  Forbidden,
+} from "../../../../shared/helpers/http/http_codes";
 import { GetAllPresencesByEventViewmodel } from "./get_all_presences_by_event_viewmodel";
 import { NoItemsFound } from "../../../../shared/helpers/errors/usecase_errors";
 import { EntityError } from "../../../../shared/helpers/errors/domain_errors";
-import { ParameterError } from "../../../../shared/helpers/http/http_codes";
 import {
   InvalidParameter,
   InvalidRequest,
