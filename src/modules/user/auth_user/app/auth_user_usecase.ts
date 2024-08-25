c5930303e5a8219c36f0bd4af5b764b4582d9f4e
LucaPinheiro <lucapgomes11@gmail.com>
2024-08-25 11:50:39 -0300

    refactor: Improve error handling in AuthUserUsecase

--- src/modules/user/auth_user/app/auth_user_usecase.ts
+++ src/modules/user/auth_user/app/auth_user_usecase.ts
@@ -3,14 +3,14 @@ import { User } from "../../../../shared/domain/entities/user";
 import { IUserRepository } from "../../../../shared/domain/repositories/user_repository_interface";
 import { EntityError } from "../../../../shared/helpers/errors/domain_errors";
 import { NoItemsFound } from "../../../../shared/helpers/errors/usecase_errors";
-import { PasswordDoesNotMatchError } from "../../../../shared/helpers/errors/login_errors";
+import { InvalidCredentialsError, PasswordDoesNotMatchError } from "../../../../shared/helpers/errors/login_errors";
 
 export class AuthUserUsecase {
   constructor(private repo: IUserRepository) {}
 
   async execute(email: string, password: string) {
     if (!email) {
-      throw new NoItemsFound(" email");
+      throw new NoItemsFound("email");
     }
 
     if (!User.isValidEmail(email)) {
@@ -23,7 +23,7 @@ export class AuthUserUsecase {
 
     const user = await this.repo.getUserByEmail(email);
     if (!user) {
-      throw new Error("User not found");
+      throw new InvalidCredentialsError();
     }
 
     const passwordMatch = await compare(password, user.password);
