cd1e42668d5532362485f8892f8bfc6a0a59a97c
LucaPinheiro <lucapgomes11@gmail.com>
2024-08-25 11:50:45 -0300

    refactor: Update import statements in update_status_controller.ts

--- src/modules/user/update_status/app/update_status_controller.ts
+++ src/modules/user/update_status/app/update_status_controller.ts
@@ -1,15 +1,25 @@
 import { Request, Response } from "express";
-import { BadRequest, Forbidden, InternalServerError } from "http-errors";
-import { MissingParameters } from "../../../../shared/helpers/errors/controller_errors";
+import {
+  ParameterError,
+  BadRequest,
+  InternalServerError,
+  Forbidden,
+} from "../../../../shared/helpers/http/http_codes";
+import {
+  InvalidParameter,
+  InvalidRequest,
+  MissingParameters,
+} from "../../../../shared/helpers/errors/controller_errors";
 import { UpdateStatusViewmodel } from "./update_status_viewmodel";
 import { UpdateStatusUsecase } from "./update_status_usecase";
 import { NoItemsFound } from "../../../../shared/helpers/errors/usecase_errors";
 import { UserFromToken } from "../../../../shared/middlewares/jwt_middleware";
+import { EntityError } from "../../../../shared/helpers/errors/domain_errors";
 
 export class UpdateStatusController {
   constructor(private updateStatusUseCase: UpdateStatusUsecase) {}
 
-  async handle(req: Request, res: Response): Promise<Response> {
+  async handle(req: Request, res: Response) {
     const userFromToken = req.user as UserFromToken;
 
     const allowedRoles = ["ADMIN", "SECRETARY", "MODERATOR"];
@@ -30,20 +40,28 @@ export class UpdateStatusController {
     const { id, status } = req.body;
 
     try {
-      const user = await this.updateStatusUseCase.execute(id, status);
+      await this.updateStatusUseCase.execute(id, status);
 
       return res
         .status(200)
         .json(new UpdateStatusViewmodel("User status updated."));
     } catch (error: any) {
-      if (
-        error instanceof BadRequest ||
-        error instanceof Forbidden ||
-        error instanceof InternalServerError
-      ) {
-        return res.status(error.status).json(error);
+      if (error instanceof InvalidRequest) {
+        new BadRequest(error.message).send(res);
       }
-      return res.status(500).json(new InternalServerError(error.message));
+      if (error instanceof InvalidParameter) {
+        new ParameterError(error.message).send(res);
+      }
+      if (error instanceof EntityError) {
+        new ParameterError(error.message).send(res);
+      }
+      if (error instanceof Forbidden) {
+        new Forbidden(error.getMessage()).send(res);
+      }
+      if (error instanceof NoItemsFound) {
+        new Forbidden(error.message).send(res);
+      }
+      new InternalServerError("Internal Server Error").send(res);
     }
   }
 }
