6570a960ad5f255a01e86b4c19439483f2155b63
LucaPinheiro <lucapgomes11@gmail.com>
2024-08-27 11:44:39 -0300

    refactor: Update CompleteCertificateDTO constructor and toJSON method

--- src/shared/infra/dto/complete_certificate_dto.ts
+++ src/shared/infra/dto/complete_certificate_dto.ts
@@ -1,28 +1,35 @@
 export class CompleteCertificateDTO {
-    presenceId: string;
-    userId: string;
-    eventId: string;
-    date: number;
-    eventName: string;
-    userName: string;
+  presenceId: string;
+  userId: string;
+  eventId: string;
+  date: number;
+  eventName: string;
+  userName: string;
 
-    constructor(presenceId: string, userId: string, eventId: string, date: number, eventName: string, userName: string) {
-        this.presenceId = presenceId;
-        this.userId = userId;
-        this.eventId = eventId;
-        this.date = date;
-        this.eventName = eventName;
-        this.userName = userName;
-    }
+  constructor(
+    presenceId: string,
+    userId: string,
+    eventId: string,
+    date: number,
+    eventName: string,
+    userName: string
+  ) {
+    this.presenceId = presenceId;
+    this.userId = userId;
+    this.eventId = eventId;
+    this.date = date;
+    this.eventName = eventName;
+    this.userName = userName;
+  }
 
-    toJSON() {
-        return {
-            presenceId: this.presenceId,
-            userId: this.userId,
-            eventId: this.eventId,
-            date: this.date,
-            eventName: this.eventName,
-            userName: this.userName,
-        }
-    }
+  toJSON() {
+    return {
+      presenceId: this.presenceId,
+      userId: this.userId,
+      eventId: this.eventId,
+      date: this.date,
+      eventName: this.eventName,
+      userName: this.userName,
+    };
+  }
 }
