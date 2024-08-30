0f2ca2f1199bd13390d449716a1a9dfdc4b11d12
LucaPinheiro <lucapgomes11@gmail.com>
2024-08-30 17:43:10 -0300

    refactor: Add route for downloading users as CSV

---
+++ src/modules/user/download_users_csv/app/download_users_csv_presenter.ts
@@ -0,0 +1,21 @@
+import express, { Request, Response } from "express";
+import { UserRepositoryPrisma } from "../../../../shared/infra/repositories/user_repository_prisma";
+import { DownloadUsersCsvUsecase } from "./download_users_csv_usecase";
+import { GetAllUsersController } from "./download_users_csv_controller";
+import { authenticateToken } from "../../../../shared/middlewares/jwt_middleware";
+
+const router = express.Router();
+
+const userRepository = new UserRepositoryPrisma();
+const getAllUsersUseCase = new DownloadUsersCsvUsecase(userRepository);
+const getAllUsersController = new GetAllUsersController(getAllUsersUseCase);
+
+router.get(
+  "/download-users",
+  authenticateToken,
+  async (req: Request, res: Response) => {
+    await getAllUsersController.handle(req, res);
+  }
+);
+
+export default router;
