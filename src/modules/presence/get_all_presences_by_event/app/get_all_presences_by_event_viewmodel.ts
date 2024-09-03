c12fbb52e10dc2b0bc8bd9738aba46f1960c9669
LucaPinheiro <lucapgomes11@gmail.com>
2024-09-02 22:17:48 -0300

    refactor: Update GetAllPresencesByEventViewmodel to include eventName and userName properties

--- src/modules/presence/get_all_presences_by_event/app/get_all_presences_by_event_viewmodel.ts
+++ src/modules/presence/get_all_presences_by_event/app/get_all_presences_by_event_viewmodel.ts
@@ -1,16 +1,19 @@
-import { Presence } from "../../../../shared/domain/entities/presence";
-
+import { PresencesFromEventDTO } from "../../../../shared/infra/dto/presences_from_event_dto"; // Importando o DTO atualizado
 
 export class GetAllPresencesByEventViewmodel {
-    presenceId: string;
-    eventId: string;
-    userId: string;
-    date: number;
-    
-    constructor(presence: Presence) {
-        this.presenceId = presence.presenceId;
-        this.eventId = presence.eventId;
-        this.userId = presence.userId;
-        this.date = presence.date;
-    }
+  presenceId: string;
+  eventId: string;
+  userId: string;
+  date: number;
+  eventName: string;
+  userName: string;
+
+  constructor(presence: PresencesFromEventDTO) {
+    this.presenceId = presence.presenceId;
+    this.eventId = presence.eventId;
+    this.userId = presence.userId;
+    this.date = presence.date;
+    this.eventName = presence.eventName;
+    this.userName = presence.userName;
+  }
 }
