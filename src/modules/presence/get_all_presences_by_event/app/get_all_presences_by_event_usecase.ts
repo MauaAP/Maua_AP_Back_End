4b37063595d07303ff54f92592a4f9f3e9ccc63a
LucaPinheiro <lucapgomes11@gmail.com>
2024-09-02 22:17:44 -0300

    refactor: Update GetAllPresencesByEventUsecase to return PresencesFromEventDTO[]

--- src/modules/presence/get_all_presences_by_event/app/get_all_presences_by_event_usecase.ts
+++ src/modules/presence/get_all_presences_by_event/app/get_all_presences_by_event_usecase.ts
@@ -1,19 +1,19 @@
-import { Presence } from "../../../../shared/domain/entities/presence";
+import { PresencesFromEventDTO } from "../../../../shared/infra/dto/presences_from_event_dto";
 import { IPresenceRepository } from "../../../../shared/domain/repositories/presence_repository_interface";
 
 export class GetAllPresencesByEventUsecase {
   constructor(private presenceRepository: IPresenceRepository) {
     this.presenceRepository = presenceRepository;
   }
-  async execute(eventId: string): Promise<Presence[]> {
+
+  async execute(eventId: string): Promise<PresencesFromEventDTO[]> {
     try {
-      const presences = await this.presenceRepository.getAllPresencesByEventId(
-        eventId
-      );
-      return presences;
+      const presencesDTO =
+        await this.presenceRepository.getAllPresencesByEventId(eventId);
+      return presencesDTO;
     } catch (error: any) {
-        console.error("Erro ao buscar presenças por evento:", error);
-        throw new Error("Erro ao buscar presenças por evento.");
+      console.error("Erro ao buscar presenças por evento:", error);
+      throw new Error("Erro ao buscar presenças por evento.");
     }
   }
 }
