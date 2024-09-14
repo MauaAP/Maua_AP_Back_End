3b37d7e1792966e26bf659996177afdef6db320a
LucaPinheiro <lucapgomes11@gmail.com>
2024-09-14 20:05:29 -0300

    feat: Add DownloadEventsCsvPresenter to handle downloading events as CSV

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -11,6 +11,7 @@ import CreateEventPresenter from "../modules/event/create_event/app/create_event
 import GetAllEventsPresenter from "../modules/event/get_all_events/app/get_all_events_presenter";
 import GetEventByIdPresenter from "../modules/event/get_event_by_id/app/get_event_by_id_presenter";
 import DeleteEventByIdPresenter from "../modules/event/delete_event_by_id/app/delete_event_by_id_presenter";
+import DownloadEventsCsvPresenter from "../modules/event/download_events_csv/app/download_events_csv_presenter";
 import CreatePresencePresenter from "../modules/presence/create_presence/app/create_presence_presenter";
 import GetAllPresencesByEventPresenter from "../modules/presence/get_all_presences_by_event/app/get_all_presences_by_event_presenter";
 import GetAllPresencesByTokenPresenter from "../modules/presence/get_all_presences_by_token/app/get_all_presences_by_token_presenter";
@@ -47,6 +48,7 @@ const routes = (app: Express) => {
   app.use("/api", GetAllEventsPresenter);
   app.use("/api", GetEventByIdPresenter);
   app.use("/api", DeleteEventByIdPresenter);
+  app.use("/api", DownloadEventsCsvPresenter);
 
   // presence routes
   app.use("/api", CreatePresencePresenter);
