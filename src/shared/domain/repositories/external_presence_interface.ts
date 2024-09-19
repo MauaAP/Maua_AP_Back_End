59f66bf841a932cf96ed54ffee95009682662ebc
LucaPinheiro <lucapgomes11@gmail.com>
2024-09-19 18:51:33 -0300

    refactor: Add getExternalPresenceById method to IExternalPresenceRepository

--- src/shared/domain/repositories/external_presence_interface.ts
+++ src/shared/domain/repositories/external_presence_interface.ts
@@ -3,4 +3,5 @@ import { ExternalPresence, Prisma } from "@prisma/client";
 export interface IExternalPresenceRepository {
   createExternalPresence(data: Prisma.ExternalPresenceUncheckedCreateInput): Promise<ExternalPresence>;
   findByEmailAndEvent(email: string, eventId: string, date: Date): Promise<ExternalPresence | null>;
+  getExternalPresenceById(id: string): Promise<ExternalPresence | null>
 }
