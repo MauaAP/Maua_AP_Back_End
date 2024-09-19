6e8687ef8f72e4dfbb1b4e91b668ab6c3f86f1dc
LucaPinheiro <lucapgomes11@gmail.com>
2024-09-19 18:50:44 -0300

    refactor: Add CreateExternalCertificatePresenter module for handling external certificate creation

---
+++ src/modules/presence/create_external_certificate/app/create_external_certificate_presenter.ts
@@ -0,0 +1,27 @@
+import express, { Request, Response } from "express";
+import { authenticateToken } from "../../../../shared/middlewares/jwt_middleware";
+import { EventRepositoryPrisma } from "../../../../shared/infra/repositories/event_repository_prisma";
+import { CreateExternalCertificateUsecase } from "./create_external_certificate_usecase";
+import { CreateExternalCertificateController } from "./create_external_certificate_controller";
+import { ExternalPresenceRepositoryPrisma } from "../../../../shared/infra/repositories/external_presence_prisma";
+
+const router = express.Router();
+const presenceRepository = new ExternalPresenceRepositoryPrisma();
+const eventRepository = new EventRepositoryPrisma();
+const createCertificateUseCase = new CreateExternalCertificateUsecase(
+  presenceRepository,
+  eventRepository
+);
+const createCertificateController = new CreateExternalCertificateController(
+  createCertificateUseCase
+);
+
+router.get(
+  "/create-external-certificate/:presenceId",
+  authenticateToken,
+  async (req: Request, res: Response) => {
+    await createCertificateController.handle(req, res);
+  }
+);
+
+export default router;
