5f22c0b6f1354681a657e004d0b43142234d51c0
LucaPinheiro <lucapgomes11@gmail.com>
2024-09-19 23:48:19 -0300

    refactor: Add error handling for empty user list in GetAllUsersToPresenceListUsecase

--- src/modules/user/get_all_users_to_presence_list/app/get_all_users_to_presence_list_usecase.ts
+++ src/modules/user/get_all_users_to_presence_list/app/get_all_users_to_presence_list_usecase.ts
@@ -1,11 +1,15 @@
 import { User } from "../../../../shared/domain/entities/user";
 import { IUserRepository } from "../../../../shared/domain/repositories/user_repository_interface";
+import { NoItemsFound } from "../../../../shared/helpers/errors/usecase_errors";
 
 export class GetAllUsersToPresenceListUsecase {
   constructor(private repo: IUserRepository) {}
 
   async execute(): Promise<User[]> {
     const users = this.repo.getAll();
+    if (!users) {
+      throw new NoItemsFound("users");
+    }
     return users;
   }
 }
