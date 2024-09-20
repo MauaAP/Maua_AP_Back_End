486d8b8dee26fce4bd4f824749cc8b675df0be26
LucaPinheiro <lucapgomes11@gmail.com>
2024-09-20 00:14:12 -0300

    refactor: Improve error handling and validation in UpdateStatusUsecase

--- src/modules/user/update_status/app/update_status_usecase.ts
+++ src/modules/user/update_status/app/update_status_usecase.ts
@@ -1,19 +1,25 @@
 import { User } from "../../../../shared/domain/entities/user";
 import { STATUS } from "../../../../shared/domain/enums/status_enum";
 import { IUserRepository } from "../../../../shared/domain/repositories/user_repository_interface";
+import { EntityError } from "../../../../shared/helpers/errors/domain_errors";
+import { NoItemsFound } from "../../../../shared/helpers/errors/usecase_errors";
 
 export class UpdateStatusUsecase {
-    constructor(private userRepository: IUserRepository) {}
+  constructor(private userRepository: IUserRepository) {}
 
-    async execute(id: string, status: STATUS): Promise<User> {
-        const user = await this.userRepository.getUserById(id);
+  async execute(id: string, status: STATUS): Promise<User> {
+    const user = await this.userRepository.getUserById(id);
 
-        if (!user) {
-            throw new Error("User not found.");
-        }
-
-        user.setStatus(status);
-
-        return this.userRepository.updateStatus(id, status);
+    if (!user) {
+      throw new NoItemsFound("user");
     }
+
+    if (User.validatestatus(status)) {
+      throw new EntityError("status");
+    }
+
+    user.setStatus(status);
+
+    return this.userRepository.updateStatus(id, status);
+  }
 }
