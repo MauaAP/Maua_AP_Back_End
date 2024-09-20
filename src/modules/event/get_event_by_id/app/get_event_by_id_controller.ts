1e68a1171bd34db65ffe8634dedb804f8da46e1d
lucapinheiro <lucapgomes11@gmail.com>
2024-09-20 15:56:58 -0300

    refactor: Improve error handling and validation in GetEventByIdController

--- src/modules/event/get_event_by_id/app/get_event_by_id_controller.ts
+++ src/modules/event/get_event_by_id/app/get_event_by_id_controller.ts
@@ -4,12 +4,14 @@ import { GetEventByIdViewmodel } from "./get_event_by_id_viewmodel";
 import {
   InvalidParameter,
   InvalidRequest,
+  MissingParameters,
 } from "../../../../shared/helpers/errors/controller_errors";
 import {
   BadRequest,
   InternalServerError,
   ParameterError,
 } from "../../../../shared/helpers/http/http_codes";
+import { NoItemsFound } from "../../../../shared/helpers/errors/usecase_errors";
 
 export class GetEventByIdController {
   constructor(private getEventByIdUsecase: GetEventByIdUsecase) {}
@@ -17,6 +19,12 @@ export class GetEventByIdController {
   async handle(req: Request, res: Response) {
     try {
       const eventId: string = req.params.eventId;
+      if (!eventId) {
+        throw new MissingParameters("Event");
+      }
+      if (!req.params) {
+        throw new InvalidRequest("Invalid Request");
+      }
       const event = await this.getEventByIdUsecase.execute(eventId);
       const viewmodel = new GetEventByIdViewmodel(event);
       return res.status(200).json(viewmodel);
@@ -26,6 +34,9 @@ export class GetEventByIdController {
       }
       if (error instanceof InvalidParameter) {
         return new ParameterError(error.message).send(res);
+      }
+      if (error instanceof NoItemsFound) {
+        return new BadRequest(error.message).send(res);
       }
       return new InternalServerError("Internal Server Error").send(res);
     }
