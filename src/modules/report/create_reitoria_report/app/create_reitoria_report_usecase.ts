5132788d9ed4d42ee8a0b6269da024f33d5e3f9e
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2024-12-17 13:31:28 -0300

    Refactor CreateReitoriaReportUsecase to use p-limit for concurrent processing of events

--- src/modules/report/create_reitoria_report/app/create_reitoria_report_usecase.ts
+++ src/modules/report/create_reitoria_report/app/create_reitoria_report_usecase.ts
@@ -13,36 +13,126 @@ import {
 } from "../../../../shared/infra/repositories/certificate_repository_s3";
 import { Event } from "../../../../shared/domain/entities/event";
 
+// export class CreateReitoriaReportUsecase {
+//   constructor(private eventRepository: IEventRepository, private presenceRepository: IPresenceRepository) {}
+
+//   async execute() {
+//     const events: Event[] = await this.eventRepository.getAll();
+
+
+//     const activities = events.map( async (event: Event) => {
+
+//       if (!event.props.eventId) {
+//         throw new NoItemsFound("Event not found");
+//       }
+//       const professors = await this.presenceRepository.countPresencesByEventId(event.props.eventId);
+//       console.log('aquiiiii' , event.eventName,  professors )
+
+
+//       const technicalStaff = event.props.manager.length;
+//       const collaborators = event.props.hostEmail.length;
+//       const students = 0;
+//       const total = professors + technicalStaff + collaborators + students;
+
+//       return {
+//         event: event.props.eventName,
+//         professors,
+//         technicalStaff,
+//         collaborators,
+//         students,
+//         total,
+//       };
+//     });
+
+//     const resolvedActivities = await Promise.all(activities);
+
+//     const grandTotal = {
+//       professors: resolvedActivities.reduce((acc, act) => acc + act.professors, 0),
+//       technicalStaff: resolvedActivities.reduce(
+//         (acc, act) => acc + act.technicalStaff,
+//         0
+//       ),
+//       collaborators: resolvedActivities.reduce(
+//         (acc, act) => acc + act.collaborators,
+//         0
+//       ),
+//       students: resolvedActivities.reduce((acc, act) => acc + act.students, 0),
+//       total: resolvedActivities.reduce((acc, act) => acc + act.total, 0),
+//     };
+
+//     const reportInfo: ReitoriaReportInfo = {
+//       totalEvents: events.length,
+//       activities: resolvedActivities,
+//       grandTotal,
+//     };
+
+//     const date = new Date().toISOString();
+
+//     const htmlString = getReitoriaReportHtml(reportInfo);
+
+//     const browser = await puppeteer.launch({
+//       headless: true,
+//       args: ["--no-sandbox"],
+//     });
+//     const page = await browser.newPage();
+//     await page.setContent(htmlString);
+
+//     const pdfBuffer = await page.pdf({ format: "A4", landscape: true });
+
+//     await browser.close();
+
+//     const reportUrl = await saveReitoriaReport(date, pdfBuffer);
+
+//     // Isso com certeza é a coisa mais gambiarra que você desenvolvedor atual do projeto verá!
+//     // Mas é o que temos para hoje, infelizmente.
+//     // Se arrumar, glória eterna para o senhor! Absolute Cinema! Adeus!
+//     const antesPontoCom = reportUrl.split(".com/")[0];
+//     const depoisPontoCom = reportUrl.split(".com/")[1];
+//     const formattedReportUrl = `${antesPontoCom}.com/relatorios-reitoria/${depoisPontoCom}`;
+
+//     return formattedReportUrl;
+//   }
+// }
+
+
+import pLimit from 'p-limit';
+
 export class CreateReitoriaReportUsecase {
-  constructor(private eventRepository: IEventRepository, private presenceRepository: IPresenceRepository) {}
+  constructor(
+    private eventRepository: IEventRepository,
+    private presenceRepository: IPresenceRepository
+  ) {}
 
   async execute() {
     const events: Event[] = await this.eventRepository.getAll();
 
+    const limit = pLimit(5); 
 
-    const activities = events.map( async (event: Event) => {
+    const activities = events.map((event) =>
+      limit(async () => {
+        if (!event.props.eventId) {
+          throw new NoItemsFound("Event ID not found");
+        }
+        const professors = await this.presenceRepository.countPresencesByEventId(
+          event.props.eventId
+        );
+        console.log('aquiiiii', event.props.eventName, professors);
 
-      if (!event.props.eventId) {
-        throw new NoItemsFound("Event not found");
-      }
-      const professors = await this.presenceRepository.countPresencesByEventId(event.props.eventId);
-      console.log('aquiiiii' , event.eventName,  professors )
+        const technicalStaff = event.props.manager.length;
+        const collaborators = event.props.hostEmail.length;
+        const students = 0;
+        const total = professors + technicalStaff + collaborators + students;
 
-
-      const technicalStaff = event.props.manager.length;
-      const collaborators = event.props.hostEmail.length;
-      const students = 0;
-      const total = professors + technicalStaff + collaborators + students;
-
-      return {
-        event: event.props.eventName,
-        professors,
-        technicalStaff,
-        collaborators,
-        students,
-        total,
-      };
-    });
+        return {
+          event: event.props.eventName,
+          professors,
+          technicalStaff,
+          collaborators,
+          students,
+          total,
+        };
+      })
+    );
 
     const resolvedActivities = await Promise.all(activities);
 
@@ -72,22 +162,19 @@ export class CreateReitoriaReportUsecase {
 
     const browser = await puppeteer.launch({
       headless: true,
-      args: ["--no-sandbox"],
+      args: ['--no-sandbox'],
     });
     const page = await browser.newPage();
     await page.setContent(htmlString);
 
-    const pdfBuffer = await page.pdf({ format: "A4", landscape: true });
+    const pdfBuffer = await page.pdf({ format: 'A4', landscape: true });
 
     await browser.close();
 
     const reportUrl = await saveReitoriaReport(date, pdfBuffer);
 
-    // Isso com certeza é a coisa mais gambiarra que você desenvolvedor atual do projeto verá!
-    // Mas é o que temos para hoje, infelizmente.
-    // Se arrumar, glória eterna para o senhor! Absolute Cinema! Adeus!
-    const antesPontoCom = reportUrl.split(".com/")[0];
-    const depoisPontoCom = reportUrl.split(".com/")[1];
+    const antesPontoCom = reportUrl.split('.com/')[0];
+    const depoisPontoCom = reportUrl.split('.com/')[1];
     const formattedReportUrl = `${antesPontoCom}.com/relatorios-reitoria/${depoisPontoCom}`;
 
     return formattedReportUrl;
