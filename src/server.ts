a64f30cad136898cb4848e47f5163a841c4c4ac5
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-02-06 22:36:45 -0300

    Add server.ts file to initialize Express app with CORS and JSON support

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import "dotenv/config";
+import app from "./app";
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running on http://localhost:${PORT} 🚀`);
+});
+
+import cors from "cors";
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+app.use(express.json());
+app.use(cors());
+
+export default app;
