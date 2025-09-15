f90b0a2812d81fea83fcaa9a7b60709cfe6ca629
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-09-14 22:22:52 -0300

    refactor: enhance user ID retrieval logic and response handling in GenerateAllCertificatesController

--- src/modules/presence/generate_all_certificates/app/generate_all_certificates_controller.ts
+++ src/modules/presence/generate_all_certificates/app/generate_all_certificates_controller.ts
@@ -3,7 +3,9 @@ import { UserFromToken } from "../../../../shared/middlewares/jwt_middleware";
 import type { GenerateAllCertificatesUsecase } from "./generate_all_certificates_usecase";
 
 export class GenerateAllCertificatesController {
-  constructor(private generateAllCertificatesUsecase: GenerateAllCertificatesUsecase) {}
+  constructor(
+    private generateAllCertificatesUsecase: GenerateAllCertificatesUsecase
+  ) {}
 
   async handle(req: Request, res: Response) {
     try {
@@ -11,9 +13,26 @@ export class GenerateAllCertificatesController {
       if (!userFromToken) {
         return res.status(403).json({ error: "No permission" });
       }
-      const userId = userFromToken.id;
-      const result = await this.generateAllCertificatesUsecase.execute(userId);
-      return res.status(201).json(result);
+      
+      let userId = "";
+
+      if (userFromToken.role === "ADMIN") {
+        userId = req.query.userId as string;
+      } else {
+        userId = userFromToken.id;
+      }
+
+      const pdfBuffer = await this.generateAllCertificatesUsecase.execute(
+        userId
+      );
+
+      res.setHeader("Content-Type", "application/pdf");
+      res.setHeader(
+        "Content-Disposition",
+        `attachment; filename="all_certificates_${userId}.pdf"`
+      );
+
+      return res.status(200).send(pdfBuffer);
     } catch (error: any) {
       return res.status(500).json({ error: error.message });
     }
